2e48e83d2f5ee00691e63592465d477fec8fb41e
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-06 16:22:57 -0400

    fixed pages

--- controller/Home-routes.js
+++ controller/Home-routes.js
@@ -1,26 +1,58 @@
 const router = require('express').Router();
 const { Movie } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
+require("dotenv").config()
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  res.render("home")
+})
+
+router.post('/', async (req, res) => {
+
   try {
-    const dbMovieData = await Movie.findAll({
-      include: [
-        {
-          model: Movie,
-          attributes: ['title', 'genre', 'description'],
-        },
-      ],
-    });
+    let searchQuery = {
+      ...req.body,
+      apikey: process.env.APIKEY
+    }
 
-    res.render('homepage', {
-      movies,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    console.log(searchQuery)
+    searchMovie(searchQuery)
+    function searchMovie(params) {
+      let queryString = new URLSearchParams(params)
+      let URL = `http://www.omdbapi.com/?${queryString}`
+      // let search = (await(await fetch(URL)).json())
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", URL, true)
+
+      // if search results has movies return first 5 movies
+      // if (search.length) {
+      //   const imdbTDArr = []
+
+      //   // grabs first 5 movie imdb IDs
+      //   for (let i = 0; i < 5; i++) {
+      //     imdbTDArr.push(search.Search[i].imdbID)
+      //   }
+      //   const requests = imdbTDArr.map(
+      //     async (id) =>
+      //       (
+      //         await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
+      //       ).json()
+      //   );
+
+      //   // sends req to fetch all movies be id
+
+      //   Promise.all(requests).then(
+      //     (res) => console.log(res)
+
+      //   )
+      // }
+    }
+
   }
-});
+  catch (error) {
+    res.json(error)
 
+  }
+})
 module.exports = router;
--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,8 @@
 // OMDB apikey
-require("dotenv").config()
 //TODO: how to use dotenv to protect our apikeys
-const APIKEY = "2aaebd7b"
 const formHandler = document.querySelector("#form-handler")
 
-function titleSearch(e) {
+async function titleSearch(e) {
     e.preventDefault();
     // listens for search bar
     let title = document.querySelector("#search-text").value.trim();
@@ -12,44 +10,25 @@ function titleSearch(e) {
     let searchQuery = {
         // search title
         s: title,
-        apikey: APIKEY,
     }
 
-    // if something is searched runs the fetch function
+    // if title exists then send a post
+
     if (title.length) {
-        searchMovie(searchQuery)
-    }
-}
+        const res = await fetch("/", {
+            method: "POST",
+            body: JSON.stringify(searchQuery),
+            headers: {
+                "content-type": "application/json",
+            },
+        })
 
-async function searchMovie(params) {
-    let queryString = new URLSearchParams(params)
-    let URL = `http://www.omdbapi.com/?${queryString}`
-    let search = (await (await fetch(URL)).json())
-
-    // if search results has movies return first 5 movies
-    if (search.length) {
-        const imdbTDArr = []
-        // grabs first 5 movie imdb IDs
-        for (let i = 0; i < 5; i++) {
-            imdbTDArr.push(search.Search[i].imdbID)
+        if (res.ok) {
+            console.log("post received")
+        } else {
+            console.log("post failed")
         }
 
-        const requests = imdbTDArr.map(
-            async (id) =>
-                (
-                    await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
-                ).json()
-        );
-
-        // sends req to fetch all movies be id
-        // TODO: how would I render these straight into handlebar? or would I send them to controller?
-
-        Promise.all(requests).then(
-            (res) => console.log(res)
-        )
-    }
-    else {
-        console.log(`Could not find any movies!}`)
     }
 
 }
