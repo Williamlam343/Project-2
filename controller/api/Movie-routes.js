73e5e7307988bec39db3e3ae3cd5c9146317c8c0
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-06 15:30:23 -0400

    updated index.js routes

--- controller/api/Movie-routes.js
+++ controller/api/Movie-routes.js
@@ -3,28 +3,30 @@ const { Movie } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/movie/:id', withAuth, async (req, res) => {
-    try {
-      const dbMovieData = await Movie.findByPk(req.params.id, {
-        include: [
-          {
-            model: Movie,
-            attributes: [
-              'id',
-              'title',
-              'release_date',
-              'created_date',
-              'reviews',
-              'genre',
-              'description',
-            ],
-          },
-        ],
-      });
-  
-      const gallery = dbMovieData.get({ plain: true });
-      res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+  try {
+    const dbMovieData = await Movie.findByPk(req.params.id, {
+      include: [
+        {
+          model: Movie,
+          attributes: [
+            'id',
+            'title',
+            'release_date',
+            'created_date',
+            'reviews',
+            'genre',
+            'description',
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbMovieData.get({ plain: true });
+    res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
