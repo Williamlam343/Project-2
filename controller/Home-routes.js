9690106bfcef1c0ef6d847887eceea86716456f5
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-10-07 03:14:59 -0400

    cards now render on homepage

--- controller/Home-routes.js
+++ controller/Home-routes.js
@@ -5,15 +5,28 @@ const axios = require("axios")
 require("dotenv").config()
 
 router.get("/", async (req, res) => {
-  res.render("home")
+
+  if (req.session.result) {
+
+    let result = req.session.result;
+    // req.session.result = null;
+    console.log(result)
+    res.render("home", { result })
+
+  }
+  else {
+
+    res.render("home")
+  }
+
 })
 
-router.post('/', async (req, res) => {
-
+router.post('/', async (req, res, next) => {
   try {
     let searchQuery = {
       ...req.body,
-      apikey: process.env.APIKEY
+      apikey: process.env.APIKEY,
+      type: "movie"
     }
     let queryString = new URLSearchParams(searchQuery)
     let URL = `http://www.omdbapi.com/?${queryString}`
@@ -25,8 +38,8 @@ router.post('/', async (req, res) => {
     // grabs first 5 movie imdb IDs
     for (let i = 0; i < 5; i++) {
       imdbTDArr.push(data.Search[i].imdbID)
-
     }
+
     const requests = imdbTDArr.map(
       (id) =>
       (
@@ -34,13 +47,16 @@ router.post('/', async (req, res) => {
       )
     );
     // sends req to fetch all movies be id
-    let MoviesList = await Promise.all(requests)
-    MoviesList.forEach((data) => console.log(data.data))
 
+    let moviesList = await Promise.all(requests)
+    req.session.result = moviesList.map((data) => data.data)
+
+    res.redirect("/")
   }
   catch (error) {
     res.json(error)
-
   }
 })
+
+
 module.exports = router;
